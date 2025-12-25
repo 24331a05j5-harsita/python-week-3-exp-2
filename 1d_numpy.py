f55ab6e79c4d9c97d115ea6c2f22bc15d588b2da
24331a05j5-harsita <24331a05j5@mvgrce.edu.in>
2025-12-25 21:21:19 +0530

    Add files via upload

---
+++ 1d_numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+arr=np.array([12,22,32,42,52,62])
+print("first element in array is:",arr[0])
+print("last element in array is:",arr[-1])
+print("second last element in array is",arr[-2])
+print("elements from 1 to 3 are:",arr[1:4])
+print("elements from start to 3 is:",arr[:4])
+print("elements from 2 to end is:",arr[:2])
+print("sum of elements in array is :",arr.sum())
+print("mean of elements in array is:",arr.mean())
+
